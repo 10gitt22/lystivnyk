363db734dcb6e2b9f7a15d39c072be4dad74704c
Yevhen Gitt <yevhengitt22@gmail.com>
2023-11-05 21:21:46 +0200

    add configs for ui lib

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,79 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
+  darkMode: ['class'],
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        border: 'var(--border)',
+        input: 'withOpacity(--input)',
+        ring: 'rgba(var(--ring), <alpha-value>)',
+        background: 'rgba(var(--background), <alpha-value>)',
+        foreground: 'rgba(var(--foreground), <alpha-value>)',
+        primary: {
+          DEFAULT: 'rgba(var(--primary), <alpha-value>)',
+          foreground: 'rgba(var(--primary-foreground), <alpha-value>)',
+        },
+        secondary: {
+          DEFAULT: 'rgba(var(--secondary), <alpha-value>)',
+          foreground: 'rgba(var(--secondary-foreground), <alpha-value>)',
+        },
+        destructive: {
+          DEFAULT: 'rgba(var(--destructive), <alpha-value>)',
+          foreground: 'rgba(var(--destructive-foreground), <alpha-value>)',
+        },
+        muted: {
+          DEFAULT: 'rgba(var(--muted), <alpha-value>)',
+          foreground: 'rgba(var(--muted-foreground), <alpha-value>)',
+        },
+        accent: {
+          DEFAULT: 'rgba(var(--accent), <alpha-value>)',
+          foreground: 'rgba(var(--accent-foreground), <alpha-value>)',
+        },
+        popover: {
+          DEFAULT: 'rgba(var(--popover), <alpha-value>)',
+          foreground: 'rgba(var(--popover-foreground), <alpha-value>)',
+        },
+        card: {
+          DEFAULT: 'rgba(var(--card), <alpha-value>)',
+          foreground: 'rgba(var(--card-foreground), <alpha-value>)',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require('tailwindcss-animate')],
+};
+
+export default config;
